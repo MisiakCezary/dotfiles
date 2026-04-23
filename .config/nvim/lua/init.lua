580b2a878e3ef1913fc414b7f5f405f799e44aed
Cezary <84717445+MisiakCezary@users.noreply.github.com>
2026-04-23 22:26:34 +0200

    Pinned plugins, added required setup() for mason-nvim-dap

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -27,17 +27,19 @@ vim.call('plug#begin')
 Plug('tomasiser/vim-code-dark', { commit = '4624dc223bf574aa9a731c2383c579847303c380'})
 Plug('airblade/vim-gitgutter', { commit = '21c977e8597c468c7dc76001389b0b430d46a4b0'})
 Plug('nvim-treesitter/nvim-treesitter', { commit = '4916d6592ede8c07973490d9322f187e07dfefac'})
-Plug('mason-org/mason.nvim')
+Plug('mason-org/mason.nvim', { commit = '12ddd182d9efbdc848b540f16484a583d52da0fb'})
 Plug('mfussenegger/nvim-dap', { commit = '45a69eba683a2c448dd9ecfc4de89511f0646b5f'})
-Plug('mason-org/mason-lspconfig.nvim')
-Plug('jay-babu/mason-nvim-dap.nvim')
+Plug('mason-org/mason-lspconfig.nvim', { commit = '0c2823e0418f3d9230ff8b201c976e84de1cb401'})
+Plug('jay-babu/mason-nvim-dap.nvim', { commit = '9a10e096703966335bd5c46c8c875d5b0690dade'})
 Plug('igorlfs/nvim-dap-view', { commit = '1dd4ba2307245ca9517a4b9d99f3bf80830e4397'})
 Plug('mfussenegger/nvim-dap-python', { commit = '1808458eba2b18f178f990e01376941a42c7f93b'})
 Plug('nvim-lua/plenary.nvim', { commit = '74b06c6c75e4eeb3108ec01852001636d85a932b'})
 Plug('nvim-telescope/telescope.nvim', { commit = '506338434fec5ad19cb1f8d45bf92d66c4917393'})
 vim.call('plug#end')
 
+-- Start mason ()
 require("mason").setup()
+require("mason-nvim-dap").setup()
 
 -- Telescope remaps
 vim.api.nvim_set_keymap('n', '<C-p>', '<Esc>:lua require"telescope.builtin".find_files({ find_command = {"rg", "--files", "--hidden", "-g", "!.git" }})<CR>', { noremap = true, silent = true })
@@ -45,6 +47,7 @@ vim.api.nvim_set_keymap('v', '<C-p>', '"9y:lua require"telescope.builtin".find_f
 vim.api.nvim_set_keymap('n', '<C-f>', '<Esc>:lua require"telescope.builtin".live_grep()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<C-f>', '"9y:lua require"telescope.builtin".live_grep({ default_text = "<C-r>9" })<CR>', { noremap = true, silent = true })
 
+-- Launch theme
 vim.cmd('colorscheme codedark')
 
 -- Autocomplete when typing
