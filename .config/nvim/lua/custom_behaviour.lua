abb98988429ed058bd20a739b1811fe35f5a8213
Cezary <84717445+MisiakCezary@users.noreply.github.com>
2026-05-29 22:20:02 +0200

    Some sane auto behaviours
    Indenting in visual mode doesn't exit it.
    Opening files put cursor where last left.

--- .config/nvim/lua/custom_behaviour.lua
+++ .config/nvim/lua/custom_behaviour.lua
@@ -1,3 +1,21 @@
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
+
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    local line = mark[1]
+    local ft = vim.bo.filetype
+    if line > 0 and line <= lcount
+      and vim.fn.index({ "commit", "gitrebase", "xxd" }, ft) == -1
+      and not vim.o.diff then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- Autocomplete when typing
 vim.api.nvim_create_autocmd("InsertCharPre", {
   pattern = "*",
