634320c11ba05a933b954024c10df4164a321516
Cezary <84717445+MisiakCezary@users.noreply.github.com>
2026-06-09 23:58:02 +0200

    Removed italic fonts from theme

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -3,8 +3,6 @@ vim.o.background = 'light'
 require('vscode').setup({
     style = 'light',
     transparent = true,
-    italic_comments = true,
-    italic_inlayhints = true,
     underline_links = true,
 })
 vim.cmd.colorscheme 'vscode'
