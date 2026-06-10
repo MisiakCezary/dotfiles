506a3c43c5899adcc332fab2b7d427a5fdb31016
Cezary <84717445+MisiakCezary@users.noreply.github.com>
2026-06-10 17:43:27 +0200

    Increased file size for syntax highlighting off

--- .config/nvim/lua/big_file.lua
+++ .config/nvim/lua/big_file.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_autocmd( {'BufReadPost', "FileReadPre"}, {
     callback = function()
         local path = vim.fn.expand('%')
         local size = vim.fn.getfsize(path)
-        if size > 512 * 1024 then
+        if size > 1024 * 1024 then
             vim.g.huge_buffer = true
             vim.opt.syntax = 'off'
             vim.opt.filetype = 'off'
