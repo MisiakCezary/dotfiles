36417db9547bcae7bf56d3ab37718713bfc05cc6
Cezary <84717445+MisiakCezary@users.noreply.github.com>
2026-07-20 20:44:11 +0200

    Restored enter functionality in quickfix list

--- .config/nvim/after/ftplugin/qf.lua
+++ .config/nvim/after/ftplugin/qf.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('n', '<C-p>', '<Up><Enter><C-W>p', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<C-n>', '<Down><Enter><C-W>p', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<Enter>', '<Enter>', { noremap = true, silent = true})
